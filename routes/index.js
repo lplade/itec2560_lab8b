e382eedaa7f9b1a03d18e21f14de472f6ab817ec
Larry Lade <lplade@users.noreply.github.com>
2016-02-24 15:09:17 -0600

    Generate output

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,30 @@ function homepage(req, res){
   res.send("Currency site");
 }
 
+function homepage(req, res){
+  res.render('index');
+}
+
+/* GET request, for form submit */
+router.get('/convert', convert);
+
+function convert(req, res){
+  //Data send as a post request is available in the
+  //body.query attribute; properties same as names in form
+
+  var dollars = req.query.dollaramount;
+  var convertTo = req.query.tocurrency;
+
+  console.log("query was: convert " + dollars + " to " + convertTo);
+
+  //Our conversion rates - TODO what's wrong with this?
+  var conversions = {"Pound":1.6, "Euro":1.1};
+
+  var conversionRate = conversions[convertTo];
+
+  var convertedVal = conversionRate * dollars;
+
+  res.render('result', {dollars:dollars, currency:convertTo, converted:convertedVal});
+}
+
 module.exports = router;
