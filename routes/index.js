97382ed77904acd8455be726c2d25da56aea5292
Larry Lade <lplade@users.noreply.github.com>
2016-02-24 19:40:50 -0600

    Complete essential assignment Added ability to choose starting currency Styled about page

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,43 @@ var router = express.Router();
 /* GET the app's home page */
 router.get('/', homepage);
 
-function homepage(req, res){
-  res.send("Currency site");
-}
+/*function homepage(req, res){
+ res.send("Currency site");
+ }*/
 
-function homepage(req, res){
-  res.render('index');
+function homepage(req, res) {
+	res.render('index');
 }
 
 /* GET request, for form submit */
 router.get('/convert', convert);
 
-function convert(req, res){
-  //Data send as a post request is available in the
-  //body.query attribute; properties same as names in form
+function convert(req, res) {
+	//Data send as a post request is available in the
+	//body.query attribute; properties same as names in form
 
-  var dollars = req.query.dollaramount;
-  var convertTo = req.query.tocurrency;
+	var money = req.query.fromamount;
+	var convertFrom = req.query.fromcurrency;
+	var convertTo = req.query.tocurrency;
 
-  console.log("query was: convert " + dollars + " to " + convertTo);
+	console.log("query was: convert " + money + " to " + convertTo);
 
-  //Our conversion rates - TODO what's wrong with this?
-  var conversions = {"Pound":1.6, "Euro":1.1};
+	//Our conversion rates - current as of 24 Feb 2016
+	var conversions = {"USD":1.0, "GBP": 0.72, "EUR": 0.91};
 
-  var conversionRate = conversions[convertTo];
+	//convert price into dollars, then into desired currency.
+	var baseUSD = money / conversions[convertFrom];
+	console.log("Internal conversion $" + baseUSD);
+	var conversionRate = conversions[convertTo];
 
-  var convertedVal = conversionRate * dollars;
+	var convertedVal = conversionRate * baseUSD;
 
-  res.render('result', {dollars:dollars, currency:convertTo, converted:convertedVal});
+	res.render('result', {
+		startmoney: money,
+		origcurrency: convertFrom,
+		currency: convertTo,
+		converted: convertedVal.toFixed(2)
+	});
 }
 
 module.exports = router;
